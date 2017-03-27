370f533d19d96c1e5307edaa7e4099a8e30c866d
PetterEmbret <pep.98@hotmail.com>
2017-03-27 11:06:53 +0200

    La inn klassen Petter
    klassen petter med metoden min

---
+++ Flaggtest design/Flaggtest design/Petter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flaggtest_design
+{
+    class Petter
+    {
+        private void buttonsjekksvar_click(object sender, EventArgs e)
+            if (pictureboxeu1.Bounds.IntersectsWith(labeleu1.Bounds))
+            label1.BackColor = System.Drawing.Color.Chartreuse;
+            else
+            label1.BackColor = System.Drawing.Color.OrangeRed;
+            //skal gjøre dette for hvert alternativ
+    }
+}
